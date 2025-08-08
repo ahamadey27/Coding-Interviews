b80b882ffa1b7c52ae805372ab19c676bf55ac73
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-08 13:22:15 -0400

    Add Wonderlog.dll and reference assembly for .NET 9.0
    - Generated Wonderlog.dll and its reference assembly for .NET 9.0.
    - Included necessary metadata and attributes for assembly identification.
    - Ensured compatibility with .NET 9.0 framework.

---
+++ Wonderlog/HighRatedHotels.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wonderlog
+{
+    
+        /*You are given a list of hotels and their ratings. Your task is to filter this list and display only the hotels that have a rating of 8.5 or higher.
+
+        Input Specification
+        The first line will contain an integer N (1leN
+        le100), the number of hotels.
+        The next N lines will each contain a hotel name (a string that may include spaces) followed by a comma, and then a floating-point number representing its rating R (1.0
+        leR
+        le10.0).
+
+        Output Specification
+        Print the names of the hotels with a rating of 8.5 or higher, each on a new line, in the same order they appeared in the input.
+        Sample Input
+
+        Sample Input
+        Grand Hyatt,8.2
+        The Peninsula,9.1
+        Four Seasons,9.4
+        Motel 6,4.5
+        Ritz Carlton,8.9
+
+        sample Output
+        The Peninsula
+        Four Seasons
+        Ritz Carlton
+        */
+    public class HighRatedHotels
+    {
+
+    }
+}
--- Wonderlog/obj/Debug/net9.0/Wonderlog.AssemblyInfo.cs
+++ Wonderlog/obj/Debug/net9.0/Wonderlog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wonderlog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36d90693e5388fc537cb75f6f7c874fd7fda3253")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a706c5f484232b7150edd4456f4ced94e7664667")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wonderlog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wonderlog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
