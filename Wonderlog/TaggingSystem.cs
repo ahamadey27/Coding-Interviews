aad8fc9db6d0ebacd92437778428801ccedb0631
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-10 15:03:30 -0400

    Add TaggingSystem class with validation logic for photo tagging

---
+++ Wonderlog/TaggingSystem.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wonderlog
+{
+    public class TaggingSystem
+    {
+        /*
+        Tagging System Validator
+        You are building a photo tagging system. A valid tag must be a single word, start with a #, contain only lowercase letters, and have a length between 2 and 15 characters (inclusive of the #).
+
+        Output Specification
+        For each potential tag, print Valid if it meets all the criteria, or Invalid if it fails any of them. Each result should be on a new line.
+
+        Sample Input
+        #travel
+        #paris2024
+        #views from the top
+        #s
+
+        Sample Output
+        Valid 
+        Invalid
+        Invalid 
+        Invalid
+        */
+    }
+}
